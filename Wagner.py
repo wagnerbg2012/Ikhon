6a52dcdc948c69668b7873e554ab5dd37f1bb3fc
wagner.bg2012 <wagner.bg2012@hotmail.com>
2023-05-13 00:36:14 -0300

    Organização de telas

--- Wagner.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-import time
-
-with sync_playwright() as p:
-    navegador = p.chromium.launch(headless=False)
-    pagina = navegador.new_page()
-    pagina.goto("https://www.google.com/")
-    time.sleep(5)
