39918ce554fba84baa1c566c4a26111637f46ba8
wagner.bg2012 <wagner.bg2012@hotmail.com>
2023-05-13 00:37:42 -0300

    Delete arquivo py de Usuario

--- SGD/main.py
+++ SGD/main.py
@@ -1,8 +1,14 @@
 from playwright.sync_api import sync_playwright
 import time
+import subprocess
+
+
 
 with sync_playwright() as p:
     navegador =p.chromium.launch(headless=False)
     pagina = navegador.new_page()
     pagina.goto("https://seplan-layout-d.ikhon.com.br/proton/proton/login.aspx")
+    subprocess.call(["python", "Faz_login.py"])
     time.sleep(5)
+
+
