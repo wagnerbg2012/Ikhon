0c041031f74588f43832aec945a09b967d59406b
Wedson32 <wedson_silva32@hotmail.com>
2023-05-17 15:58:43 -0300

    Continuação e termino da realização do inclusao do cadastro

--- SGD/BotSGD/BotSGD/bot.py
+++ SGD/BotSGD/BotSGD/bot.py
@@ -21,6 +21,7 @@ Please refer to the documentation for more information at https://documentation.
 """
 
 from botcity.core import DesktopBot
+import time
 # Uncomment the line below for integrations with BotMaestro
 # Using the Maestro SDK
 # from botcity.maestro import *
@@ -42,71 +43,112 @@ class Bot(DesktopBot):
             self.not_found("CPF")
         self.click()
         self.paste("023.020.731-69")
-        
+        time.sleep(1)
+        # Wfs- tela inicial- informar CPF_17/05/2023
         if not self.find( "senha", matching=0.97, waiting_time=10000):
             self.not_found("senha")
         self.click()
         self.paste("1234")
-        
+        time.sleep(1)
+        # Wfs- tela inicial- informar senha_17/05/2023
         if not self.find( "entrar", matching=0.97, waiting_time=10000):
             self.not_found("entrar")
         self.click()
-        
-        
+        time.sleep(1)        
+        # Wfs- tela inicial- entrar no sistema _17/05/2023
         if not self.find( "documentos", matching=0.97, waiting_time=10000):
             self.not_found("documentos")
         self.click()
-        
+        time.sleep(1)
+        # Wfs- clicar no campo "Documentos" _17/05/2023
         if not self.find( "incluir", matching=0.97, waiting_time=10000):
             self.not_found("incluir")
         self.click()
-        
-        
+        time.sleep(1)        
+        # Wfs- clicar no campo"incluir"_17/05/2023
         if not self.find( "assunto", matching=0.97, waiting_time=10000):
             self.not_found("assunto")
         self.click()
         self.paste("dsafsadsadsdfdsfsd")
-        
+        time.sleep(1)
+        # Wfs- informar o assunto na descrição do campo_17/05/2023
         if not self.find( "especie", matching=0.97, waiting_time=10000):
             self.not_found("especie")
         self.click()
-        
+        time.sleep(1)
+        # Wfs- selecionar a caixa de espécie documental_17/05/2023
         if not self.find( "alvara", matching=0.97, waiting_time=10000):
             self.not_found("alvara")
         self.click()
-        
+        time.sleep(1)
+        # Wfs- selecionar o tipo de espécie documental_17/05/2023
         if not self.find( "classificacao", matching=0.97, waiting_time=10000):
             self.not_found("classificacao")
         self.click()
-        
+        time.sleep(1)
+        # Wfs- selecionar o campo de classificação arquivistica_17/05/2023
         if not self.find( "relatorio", matching=0.97, waiting_time=10000):
             self.not_found("relatorio")
         self.click()
-        
+        time.sleep(1)
+        # Wfs- selecionar o tipo de classificação arquivistica_17/05/2023
         if not self.find( "interessados", matching=0.97, waiting_time=10000):
             self.not_found("interessados")
         self.click()
-        
+        time.sleep(1)
+        # Wfs- selecionar o campo de interessados_17/05/2023
         if not self.find( "nome", matching=0.97, waiting_time=10000):
             self.not_found("nome")
         self.click()
+        time.sleep(1)
         self.paste("wedson silva")
-        
+        time.sleep(1)
+        # Wfs- clicar no campo e escrever o nome do interessado_17/05/2023
         if not self.find( "pesquisar", matching=0.97, waiting_time=10000):
             self.not_found("pesquisar")
         self.click()
-        
+        time.sleep(1)
+        # Wfs- clicar no botão de pesquisa_17/05/2023
         if not self.find( "selecionar", matching=0.97, waiting_time=10000):
             self.not_found("selecionar")
         self.click()
-        
+        time.sleep(1)
+        # Wfs- Selecionar o nome digitado no campo de pesquisa_17/05/2023
         if not self.find( "incluir usuario", matching=0.97, waiting_time=10000):
             self.not_found("incluir usuario")
         self.click()
-        
-        
+        time.sleep(1)
+        # Wfs- incluir o usuário_17/05/2023
+        if not self.find( "escolher arquivo", matching=0.97, waiting_time=10000):
+            self.not_found("escolher arquivo")
+        self.click()
+        time.sleep(1)
+        # Wfs- clicar no botão arquivo digital_17/05/2023
+        if not self.find( "selecionar arquivo", matching=0.97, waiting_time=10000):
+            self.not_found("selecionar arquivo")
+        self.click()
+        time.sleep(1)
+        # Wfs- Selecionar o tipo de arquivo digital_17/05/2023
+        if not self.find( "abrir arquivo", matching=0.97, waiting_time=10000):
+            self.not_found("abrir arquivo")
+        self.click()
+        time.sleep(1)
+        # Wfs- escolher o arquivo degital dentro de documentos_17/05/2023
+        if not self.find( "adicionar arquivo", matching=0.97, waiting_time=10000):
+            self.not_found("adicionar arquivo")
+        self.click()
+        time.sleep(1)
+        # Wfs- Selecionar o arquivo digital_17/05/2023
+        if not self.find( "carregar pagina", matching=0.97, waiting_time=10000):
+            self.not_found("carregar pagina")                  
+        self.scroll_down(1000)
+        time.sleep(1)
+        # Wfs- Função adicionada para carregar a pagina e rolar para baixo_17/05/2023
 
-       
+        if not self.find( "incluir o documento", matching=0.97, waiting_time=10000):
+            self.not_found("incluir o documento")
+        self.click()
+        #Incluir o documento no sistema.
         
         # Uncomment to mark this task as finished on BotMaestro
         # self.maestro.finish_task(
